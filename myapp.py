95db3a1d8e94f621e311428023a642b7b8a7f661
alialabadla <aliabadllah@gmail.com>
2026-01-05 12:34:44 +0200

    update

--- myapp.py
+++ myapp.py
@@ -1,17 +1,6 @@
+
+=======
 import streamlit as st
 import ccxt
-
-st.title("Crypto Data Live")
-
-# الاتصال بمنصة Kraken (تعمل عادة بدون قيود جغرافية)
-try:
-    exchange = ccxt.kraken() 
-    symbol = 'BTC/USDT'
-    ticker = exchange.fetch_ticker(symbol)
-    price = ticker['last']
-    
-    st.metric(label=f"سعر {symbol} (Kraken)", value=f"${price:,.2f}")
-    st.success("تم جلب البيانات بنجاح!")
-except Exception as e:
-    st.error(f"حدث خطأ: {e}")
-
+st.title("THIS IS MY FIRST APP")
+st.siderbar.title("Chose Currency")
